013d65c7f41e1ff32fb142fb0a7e919530f2fe7a
Rohlend <gskosh1w@gmail.com>
2023-04-06 19:38:46 +0300

    first spring app

---
+++ src/main/java/ru/vlados/spring/controllers/DealershipController.java
@@ -0,0 +1,44 @@
+package ru.vlados.spring.controllers;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Controller
+@RequestMapping("/dealer")
+public class DealershipController implements InitializingBean {
+    private Map<String,Integer> cars = new HashMap<>();
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        cars.put("Porsche",10_000_000);
+        cars.put("Mercedes",15_000_000);
+        cars.put("BMW",8_000_000);
+        cars.put("Lada",1_000_000);
+        cars.put("Tesla",3_000_000);
+    }
+    @GetMapping("")
+    public String deal(Model model){
+        model.addAttribute("dealer","huinya");
+        return "dealer/dealer";
+    }
+    @GetMapping("/buy")
+    public String buy(@RequestParam(value = "car",required = false) String car, Model model){
+        model.addAttribute("dealer","huinya");
+        if(cars.containsKey(car)){
+            model.addAttribute("price",cars.get(car));
+            model.addAttribute("car",car);
+            return "dealer/price";
+        }
+        return "dealer/dealer";
+    }
+
+}
---
+++ src/main/java/ru/vlados/spring/dao/PersonDAO.java
@@ -0,0 +1,26 @@
+package ru.vlados.spring.dao;
+
+import org.springframework.stereotype.Component;
+import ru.vlados.spring.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private int count_people = 1;
+    private List<Person> people;
+    {
+        people = new ArrayList<>();
+        people.add(new Person(count_people++,"Tom"));
+        people.add(new Person(count_people++,"Alex"));
+        people.add(new Person(count_people++,"John"));
+        people.add(new Person(count_people++,"Ivan"));
+    }
+    public List<Person> index(){
+        return people;
+    }
+    public Person show(int id){
+        return people.stream().filter(e -> e.getId() == id).findAny().orElse(null);
+    }
+}
